f9f4da9021367efecb430a3d25267fdfe0cabb7b
Dmitro Kondratiuk <d.kondratyuk@clearjunction.com>
2024-07-09 13:03:16 +0300

    add positions

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [UserController::class, 'index'])->name('user');
 Route::get('/user/getUsers', [UserController::class, 'getUsers'])->name('user.getUsers');
 Route::get('/user/getPositionsUsers', [UserController::class, 'getPositionsUsers'])->name('user.getPositionsUsers');
-Route::get('/user/{user}', [UserController::class, 'show'])->name('user.show');
 Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
+Route::get('/user/{user}', [UserController::class, 'show'])->name('user.show');
 Route::post('/user/add', [UserController::class, 'add'])->name('user.add');
